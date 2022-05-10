2e6437a49956ae1df3eb04e795662bc3551594d4
MoidulHasan <antu.khan.988@gmail.com>
2022-05-11 01:09:28 +0600

    customized select option created

--- src/components/singleSelectOption/singleSelectOption.js
+++ src/components/singleSelectOption/singleSelectOption.js
@@ -1,18 +1,20 @@
 import React from "react";
 // import { From } from "react-bootstrap";
 import './styles.css'
+import { BsFillCheckCircleFill } from "react-icons/bs";
 
 const SingleSelectOption = (props) => {
-    let { lavel, selected } = props;
-    return (
-        <button className="option text-center border-0">
-            <h5>{lavel}</h5>
+    let { name, value, lavel, selected } = props;
 
-            <i class="fa fa-circle-check"></i>
-            <i class="fa-light fa-circle-check"></i>
-            <i className="fa fa-angle-left fs-2 text-info fw-3" />
-        </button>
+    let select = (selected === value) ? true : false;
+    console.log(select);
+
+    return (
+        <div className={`col-5 col-md-3 d-flex justify-content-center align-items-center text-center border-0 my-3`}>
+            <label for={value} className="option m-0 d-flex justify-content-center align-items-center">{lavel} {select && <BsFillCheckCircleFill className={`text-info fs-3 position-absolute top-0 end-0 m-1`} />} </label>
+            <input id={value} className={`radio-custom m-5`} type="radio" value={value} name={name} />
+        </div>
     );
-}
+};
 
 export default SingleSelectOption;
--- src/view/survey-form/step1/step1.js
+++ src/view/survey-form/step1/step1.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import SingleSelectOption from "../../../components/singleSelectOption/singleSelectOption";
 import { Form } from "react-bootstrap";
 
 const Step1 = () => {
+    const onChangeValue = (event) => {
+        console.log(event.target.value);
+        SetbusinessIndustry(event.target.value);
+    }
 
+    const [businessIndustry, SetbusinessIndustry] = useState('Defence');
 
+    const options = [
+        'Advanced manufacturing',
+        'Advanced technologies',
+        'Agribusiness & food',
+        'Arts, culture & creative',
+        'Cyber security',
+        'Defence',
+        'Education',
+        'Financial & professional services',
+        'Infrastructure & construction',
+        'Medical technology',
+        'Mining & resources',
+        'Renewable energy & sustainability',
+        'Retail services',
+        'Space',
+        'Tourism',
+        'Other'
+    ];
 
     return (
-        <SingleSelectOption lavel="Advanced manufacturing" />
+        <div>
+            <h1 className={`text-center`}>What industry is your business?</h1>
+            <div className="d-flex flex-wrap align-items-center justify-content-center px-5 mx-5" onChange={onChangeValue}>
+                {
+                    options.map((option) => (
+                        <SingleSelectOption name="business-industry" value={option} lavel={option} selected={businessIndustry} />
+                    ))
+                }
+            </div>
+        </div>
+
     );
 };
 
